79f253f87d69ece79016603e40c4692ad23d8bc6
reets <117296082+reets93@users.noreply.github.com>
2023-01-22 12:46:48 +0000

    formatting of text area working better

---
+++ assets/js/moment.js
@@ -0,0 +1,27 @@
+    // update time slots 
+    var time0 = moment("9","h a");
+    $('#t0').text(time0.format("h a"))
+    
+    var time1 = moment("10","h a");
+    $('#1').text(time1.format("h a"))
+    
+    var time2 = moment("11","h a");
+    $('#2').text(time2.format("h a"))
+    
+    var time3 = moment("12","h a");
+    $('#3').text(time3.format("h a"))
+    
+    var time4 = moment("13","h a");
+    $('#4').text(time4.format("h a"))
+    
+    var time5 = moment("14","h a");
+    $('#5').text(time5.format("h a"))
+    
+    var time6 = moment("15","h a");
+    $('#6').text(time6.format("h a"))
+    
+    var time7 = moment("16","h a");
+    $('#7').text(time7.format("h a"))
+    
+    var time8 = moment("17","h a");
+    $('#8').text(time8.format("h a"))
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,82 @@ var currentDate = moment(); // 24 March 2023
 $("#currentDay").text(currentDate.format("dddd Do MMMM YYYY")).css('color','orange')
 
 
+
 // Present timeblocks for standard business hours when the user scrolls down.
 var businessHours = 8
-
 for (i=0; i<businessHours; i++) {
-    $( "#9" ).clone().appendTo( ".container" );
+    $( "#0" ).clone().appendTo( "#timeBlockContainer" ).attr("id", "0"+(i+1)).children().eq(0).children().eq(0).attr("id",i+1);
 }
+
+var time0 = moment("9","h a");
+$('#t0').text(time0.format("h a"))
+colorCode(time0, "#t0")
+
+var time1 = moment("10","h a");
+$('#1').text(time1.format("h a"))
+colorCode(time1, "#1")
+
+var time2 = moment("11","h a");
+$('#2').text(time2.format("h a"))
+colorCode(time2, "#2")
+
+var time3 = moment("12","h a");
+$('#3').text(time3.format("h a"))
+colorCode(time3, "#3")
+
+var time4 = moment("13","h a");
+$('#4').text(time4.format("h a"))
+colorCode(time4, "#4")
+
+
+var time5 = moment("14","h a");
+$('#5').text(time5.format("h a"))
+colorCode(time5, "#5")
+
+
+var time6 = moment("15","h a");
+$('#6').text(time6.format("h a"))
+colorCode(time6, "#6")
+
+var time7 = moment("16","h a");
+$('#7').text(time7.format("h a"))
+colorCode(time7, "#7")
+
+var time8 = moment("17","h a");
+$('#8').text(time8.format("h a"))
+colorCode(time8, "#8")
+
+
+
+// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+var textArea = document.querySelectorAll("textarea")
+function colorCode(time, block) { // block = the row id
+if (time >= moment()) {
+    $(block).children().eq(1).attr("class",".past")
+    console.log("isbefore")
+} else if (time <= moment()) {
+    $(block).children().eq(1).attr("class",".future")
+    console.log("future")
+} else if (time == moment()) {
+    $(block).children().eq(1).attr("class",".present")
+    console.log("now")
+}
+}
+
+$("#0").children().eq(1).children().attr("class","past")
+
+
+// if (#timeblock.isbefore() {
+    //change to grey}
+    // else if (#timeblock.isafter()) {
+    //change to green
+//     } else {
+    //change to red
+// }
+// })
+
+
+// momentObj.isBefore()
+
+// Or in the future:
+// momentObj.isAfter()
